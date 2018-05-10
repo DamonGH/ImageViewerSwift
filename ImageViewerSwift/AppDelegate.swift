0fa0fa351d389251641e8dfe1ea7c3752daa4946
lip <289882046@qq.com>
2018-05-10 15:45:30 -0700

    Add files

---
+++ ImageViewerSwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ImageViewerSwift
+//
+//  Created by lip on 2018/5/10.
+//  Copyright © 2018年 lip. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ImageViewerSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImageViewerSwift
+//
+//  Created by lip on 2018/5/10.
+//  Copyright © 2018年 lip. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
